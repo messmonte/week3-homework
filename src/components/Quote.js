adafee15a90b1185a8e78ec9f0645c19fb2987c4
messmonte <amandamariamonte@gmail.com>
2020-02-15 01:17:51 +0100

    at the part where I have to change text colour

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 
 export default class Quote extends Component {
+  state = {
+    likeCounter: 0,
+    dislikeCounter: 0
+  };
+  //
+  // likeQuote = quoteText => {
+  //   return (quoteText.style = "font-style=bold;color:green;");
+  // };
+  // //
+  // dislikeQuote = text => {
+  //   return ();
+  // };
+  //
   render() {
     return (
       <div>
-        <p>
+        <p id="quoteParagraph">
           {this.props.quoteText} <br></br>by {this.props.quoteAuthor}
+          <button onClick={(this.props.quoteText.style = "green")}>:)</button>
+          <button onClick={(this.props.quoteText.style = "red")}>:(</button>
         </p>
       </div>
     );
